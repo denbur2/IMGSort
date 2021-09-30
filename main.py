6dec53cd53671daeef94f5033fab3be6406e98ee
Handkep <paulus.handke@gmail.com>
2021-10-01 00:10:13 +0200

    ist now useable

--- main.py
+++ main.py
@@ -9,64 +9,60 @@ import time
 
 startTime = time.time()
 
+# os.symlink(os.path.abspath("IMG_20200725_184755.jpg"), "sorted/test.jpg")
+
 directory = 'images'
 dates = []
+filenames = []
 # iterate over files in
 # that directory
 ypoints = np.array([] ,dtype=np.int_)
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    # checking if it is a file
-    if os.path.isfile(f):
-        with Image.open(f) as im:
-            # im = Image.open("testimg.jpg")
-            # im.show()
 
-            img_exif_data = im.getexif()
+for root, dirs, files in os.walk(directory):
+    # print("root:%s dirs:%s file:%s"%(root, dirs, files))
+    for filename in files:
+        print("root:%s dirs:%s file:%s"%(root, dirs, os.path.join(root, filename)))
+        filenames.append(os.path.join(root, filename))
+        f = os.path.join(root, filename)
+        # checking if it is a file
 
-            # print(img_exif_data)
+# def extractDataFromImages(f):
+        if os.path.isfile(f):
+            print(f)
+            try:
+                with Image.open(f) as im:
+                    img_exif_data = im.getexif()
+                    # for plotting results
+                    buf = {f:[], "Data":[]}
 
-            buf = {f:[], "Data":[]}
+                    for id in img_exif_data:
 
-            for id in img_exif_data:
+                        tag_name = TAGS.get(id, id)
+                        data = img_exif_data.get(id)
+                        # buf.append(tag_name)
+                        # buf.append(data)
 
-                tag_name = TAGS.get(id, id)
-                data = img_exif_data.get(id)
-                # buf.append(tag_name)
-                # buf.append(data)
+                        if id == 0x9003:
+                                buf[f].append(tag_name)
+                                buf["Data"].append(data)
+                                dates.append(str(datetime.strptime(data,"%Y:%m:%d %H:%M:%S").year) + "-" + str(datetime.strptime(data,"%Y:%m:%d %H:%M:%S").month))
+                                ypoints = np.append(ypoints, int(data.rsplit(" ")[1].rsplit(":")[1]))
 
-                if id == 0x9003:
-                        buf[f].append(tag_name)
-                        buf["Data"].append(data)
-                        dates.append(str(datetime.strptime(data,"%Y:%m:%d %H:%M:%S").year) + "-" + str(datetime.strptime(data,"%Y:%m:%d %H:%M:%S").month))
-                        # print(datetime.strptime(data,"%Y:%m:%d %H:%M:%S"))
-                        # print(datetime.strptime(data,"%Y:%m:%d %H:%M:%S").month)
-                        ypoints = np.append(ypoints, int(data.rsplit(" ")[1].rsplit(":")[1]))
-                        # print(type(int(data.rsplit(" ")[1].rsplit(":")[1])))
+                    df = pd.DataFrame(buf)
+                    df.head(10).style.format({"BasePay": "${:20,.0f}", 
+                                "OtherPay": "${:20,.0f}", 
+                                "TotalPay": "${:20,.0f}",
+                                "TotalPayBenefits":"${:20,.0f}"})
+                    # df.style
+                    # print(df.to_string())
+                    # print(df)
+            except Exception as e:
+                print(e)
+print("filenames:%s"%(filenames))
+# countarr = np.bincount(ypoints)
+# plt.plot(countarr, 'X--r')
+# plt.show()
 
-            df = pd.DataFrame(buf)
-            df.head(10).style.format({"BasePay": "${:20,.0f}", 
-                          "OtherPay": "${:20,.0f}", 
-                          "TotalPay": "${:20,.0f}",
-                          "TotalPayBenefits":"${:20,.0f}"})
-            # df.style
-            # print(df.to_string())
-            # print(df)
-# 
-# print( os.getlogin()+' is logged in')
-
-#countarr = np.bincount(ypoints)
-#print(countarr)
-#print(ypoints)
-#plt.plot(countarr, 'X--r')
-#plt.show()
-
-# src = 'images'
-# dst = 'symlinks/asdf'
-
-# This creates a symbolic link on python in tmp directory
-# os.symlink(src, dst)
-# os.mkdir('test') 
 
 def make_dirs(head):
     head_backup = head
@@ -111,18 +107,44 @@ def create_path(date):
     path = os.path.join(path, switch.get(int(date.rsplit("-")[1]),"0"))
     return path
 
-# print(make_dirs(''))
-# print(dates)
+def createDirsFromDates(dates):
+    for i in range(len(dates)):
+        try:
+            os.makedirs(create_path(dates[i]),exist_ok=True)
+            print(create_path(dates[i]))
+        except OSError as e:
+            print(e)
+
+def linkFilesInDirs(unsortedImagesPath):
+    print("==================")
+    for f in filenames:
+        # checking if it is a file
+        if os.path.isfile(f):
+            with Image.open(f) as im:
+                img_exif_data = im.getexif()
+                buf = {f:[], "Data":[]}
+                print(f)
+                for id in img_exif_data:
+
+                    tag_name = TAGS.get(id, id)
+                    data = img_exif_data.get(id)
+
+                    if id == 0x9003:
+                        src = os.path.abspath(f)
+                        date = str(datetime.strptime(data,"%Y:%m:%d %H:%M:%S").year) + "-" + str(datetime.strptime(data,"%Y:%m:%d %H:%M:%S").month)
+                        dest = os.path.join(create_path(date), os.path.split(f)[1])
+                        print("img src: %s dest: %s "%(src,dest))
+                        try:
+                            os.symlink(src, dest)
+                        except Exception as e:
+                            print(e)
+
 dates = list(set(dates))
 dates.sort()
 print(dates)
+createDirsFromDates(dates)
+linkFilesInDirs(directory)
 
-for i in range(len(dates)):
-    try:
-        os.makedirs(create_path(dates[i]),exist_ok=True)
-        print(create_path(dates[i]))
-    except OSError as e:
-        print(e)
 
 
 print('Execution time in seconds: ' + str((time.time() - startTime)))
